1a0a777c76170e790793f116270de6cc306f6ef4
Itai Tenenbaum <taitulism@gmail.com>
2020-10-07 15:46:57 +0300

    infra: bundle names, pksj prepare script, pkjs files prop

--- rollup.prod.config.js
+++ rollup.prod.config.js
@@ -1,14 +1,24 @@
+// import {terser} from 'rollup-plugin-terser';
+
 export default [{
 	input: 'resizable.js',
 	output: {
-		file: 'dist/resizable.esm-bundle.js',
+		file: 'dist/resizable.esm.js',
 		format: 'es',
 	},
-},{
+}, {
 	input: 'resizable.js',
 	output: {
 		file: 'dist/resizable.browser.js',
 		format: 'iife',
 		name: 'resizable',
 	},
+// }, {
+// 	input: 'resizable.js',
+// 	plugins: [terser()],
+// 	output: {
+// 		file: 'dist/resizable.browser.min.js',
+// 		format: 'iife',
+// 		name: 'resizable',
+// 	},
 }];
