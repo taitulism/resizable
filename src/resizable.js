ebc5a71618f41bd9858e511c5c2d51ffb350172e
Itai Tenenbaum <taitulism@gmail.com>
2020-10-17 15:50:01 +0300

    encapsulate + es6 class syntax

--- src/resizable.js
+++ src/resizable.js
@@ -1,4 +1,5 @@
-/* eslint-disable indent, no-magic-numbers */
+/* eslint-disable no-invalid-this */
+
 import getGripCreator from './get-grip-creator';
 import bindListener from './bind-listener';
 import {direction} from './grips-definitions';
@@ -14,130 +15,108 @@ const MOUSE_UP = 'mouseup';
 
 const px = 'px';
 
-export default function Resizable (elm, opts = {}) {
-	this.minWidth = opts.minWidth || 0;
-	this.minHeight = opts.minHeight || 0;
-	this.elm = elm;
-	this.isResizable = true;
-	this.originalPosition = elm.style.position || null;
-	this.destructionQueue = [];
-	this.unbindMouseMove = null;
-	this.classname = opts.classname || RESIZABLE;
-	this.grip = null;
-	this.grips = {};
+export default class Resizable {
+	constructor  (elm, opts = {}) {
+		this.minWidth = opts.minWidth || 0;
+		this.minHeight = opts.minHeight || 0;
+		this.elm = elm;
+		this.isResizable = true;
+		this.originalPosition = elm.style.position || null;
+		this.destructionQueue = [];
+		this.unbindMouseMove = null;
+		this.classname = opts.classname || RESIZABLE;
+		this.grip = null;
+		this.grips = {};
+		this.events = createEventsObj();
 
-	this.events = {
-		startResize: [],
-		resizing: [],
-		stopResize: [],
-	};
+		initMethods(this);
+		initGrips(this, opts.gripSize, opts.direction);
+		initElm(this, elm);
+	}
 
-	this.initMethods();
-	this.initGrips(opts.gripSize, opts.direction);
-	this.initElm(elm);
+	disable () {
+		this.isResizable = false;
+		this.elm.classList.add(RESIZE_DISABLED);
+		this.forEachGrip((grip) => {
+			grip.style.display = 'none';
+		});
+		return this;
+	}
+
+	enable () {
+		this.isResizable = true;
+		this.elm.classList.remove(RESIZE_DISABLED);
+		this.forEachGrip((grip) => {
+			grip.style.display = '';
+		});
+		return this;
+	}
+
+	showGrips () {
+		return this.forEachGrip((grip) => {
+			grip.style.opacity = '1';
+		});
+	}
+
+	hideGrips () {
+		return this.forEachGrip((grip) => {
+			grip.style.opacity = '0';
+		});
+	}
+
+	forEachGrip (callback) {
+		if (this.grip) {
+			callback(this.grip);
+		}
+		else {
+			callback(this.grips.topLeft);
+			callback(this.grips.topRight);
+			callback(this.grips.bottomRight);
+			callback(this.grips.bottomLeft);
+		}
+
+		return this;
+	}
+
+	on (eventName, callback) {
+		const lowerEventName = eventName.toLowerCase();
+		if (lowerEventName.includes('start')) {
+			this.events.startResize.push(callback);
+		}
+		else if (lowerEventName.includes('ing')) {
+			this.events.resizing.push(callback);
+		}
+		else if (lowerEventName.includes('end') || lowerEventName.includes('stop')) {
+			this.events.stopResize.push(callback);
+		}
+		return this;
+	}
+
+	updateElm ({width, height, top, left}) {
+		const elmStyle = this.elm.style;
+
+		if (width) elmStyle.width = width + px;
+		if (height) elmStyle.height = height + px;
+		if (top) elmStyle.top = top + px;
+		if (left) elmStyle.left = left + px;
+	}
+
+	destroy () {
+		destroyGrips(this);
+
+		this.elm.classList.remove(this.classname, RESIZING);
+		if (this.originalPosition) {
+			this.elm.style.position = this.originalPosition;
+		}
+
+		this.events = null;
+		this.elm = null;
+	}
 }
 
-Resizable.prototype.initMethods = function initMethods () {
-	this.onResizeStart = this.onResizeStart.bind(this);
-	this.onDrop = this.onDrop.bind(this);
-};
+/* ---------------------------------------------------------------------------------------------- */
 
-Resizable.prototype.initGrips = function initGrips (gripSize, gripDirection) {
-	const createGrip = getGripCreator(gripSize || 10);
-
-	if (gripDirection) {
-		const {gripElm, moveHandler} = createGrip(direction[gripDirection]);
-		this.grip = gripElm;
-		this.elm.appendChild(gripElm);
-
-		const unbind = bindListener(gripElm, MOUSE_DOWN, (ev) => {
-			this.onResizeStart(ev, moveHandler);
-		});
-
-		this.destructionQueue.push(unbind);
-	}
-	else {
-		[
-			direction.topLeft,
-			direction.topRight,
-			direction.bottomRight,
-			direction.bottomLeft,
-		].forEach((gripDir) => {
-			const {propName, gripElm, moveHandler} = createGrip(gripDir);
-			this.grips[propName] = gripElm;
-			this.elm.appendChild(gripElm);
-
-			const unbind = bindListener(gripElm, MOUSE_DOWN, (ev) => {
-				this.onResizeStart(ev, moveHandler);
-			});
-
-			this.destructionQueue.push(unbind);
-		});
-	}
-};
-
-Resizable.prototype.initElm = function initElm (elm) {
-	const position = elm.style.position || window.getComputedStyle(elm).position;
-	const box = elm.getBoundingClientRect();
-
-	if (position !== 'absolute') {
-		elm.style.position = 'absolute';
-		elm.style.top = box.top + px;
-		elm.style.left = box.left + px;
-	}
-
-	if (box.width < this.minWidth) {
-		elm.style.width = this.minWidth + px;
-	}
-
-	if (box.height < this.minHeight) {
-		elm.style.height = this.minHeight + px;
-	}
-
-	elm.classList.add(this.classname);
-};
-
-Resizable.prototype.showGrips = function showGrips () {
-	return this.forEachGrip((grip) => {
-		grip.style.opacity = '1';
-	});
-};
-
-Resizable.prototype.hideGrips = function hideGrips () {
-	return this.forEachGrip((grip) => {
-		grip.style.opacity = '0';
-	});
-};
-
-Resizable.prototype.forEachGrip = function forEachGrip (callback) {
-	if (this.grip) {
-		callback(this.grip);
-	}
-	else {
-		callback(this.grips.topLeft);
-		callback(this.grips.topRight);
-		callback(this.grips.bottomRight);
-		callback(this.grips.bottomLeft);
-	}
-
-	return this;
-};
-
-Resizable.prototype.on = function on (eventName, callback) {
-	const lowerEventName = eventName.toLowerCase();
-	if (lowerEventName.includes('start')) {
-		this.events.startResize.push(callback);
-	}
-	else if (lowerEventName.includes('ing')) {
-		this.events.resizing.push(callback);
-	}
-	else if (lowerEventName.includes('end') || lowerEventName.includes('stop')) {
-		this.events.stopResize.push(callback);
-	}
-	return this;
-};
-
-Resizable.prototype.onResizeStart = function onResizeStart (startEvent, moveHandler) {
+function onResizeStart (startEvent, moveHandler) {
 	if (!this.isResizable) return;
 
 	this.elm.classList.add(RESIZING);
@@ -163,18 +142,9 @@ Resizable.prototype.onResizeStart = function onResizeStart (startEvent, moveHand
 
 	document.addEventListener(MOUSE_UP, this.onDrop);
 	this.events.startResize.forEach(cb => cb(startEvent));
-};
+}
 
-Resizable.prototype.updateElm = function updateElm ({width, height, top, left}) {
-	const elmStyle = this.elm.style;
-
-	if (width) elmStyle.width = width + px;
-	if (height) elmStyle.height = height + px;
-	if (top) elmStyle.top = top + px;
-	if (left) elmStyle.left = left + px;
-};
-
-Resizable.prototype.onDrop = function onDrop (dropEvent) {
+function onDrop (dropEvent) {
 	this.elm.classList.remove(RESIZING);
 	this.unbindMouseMove();
 	this.unbindMouseMove = null;
@@ -182,43 +152,85 @@ Resizable.prototype.onDrop = function onDrop (dropEvent) {
 
 	const newBox = this.elm.getBoundingClientRect();
 	this.events.stopResize.forEach(cb => cb(dropEvent, newBox));
-};
+}
 
-Resizable.prototype.disable = function disable () {
-	this.isResizable = false;
-	this.elm.classList.add(RESIZE_DISABLED);
-	this.forEachGrip((grip) => {
-		grip.style.display = 'none';
-	});
-	return this;
-};
+/* ---------------------------------------------------------------------------------------------- */
 
-Resizable.prototype.enable = function enable () {
-	this.isResizable = true;
-	this.elm.classList.remove(RESIZE_DISABLED);
-	this.forEachGrip((grip) => {
-		grip.style.display = '';
-	});
-	return this;
-};
+function createEventsObj () {
+	return {
+		startResize: [],
+		resizing: [],
+		stopResize: [],
+	};
+}
 
-Resizable.prototype.destroy = function destroy () {
-	this.destroyGrips();
+function initMethods (rsz) {
+	rsz.onResizeStart = onResizeStart.bind(rsz);
+	rsz.onDrop = onDrop.bind(rsz);
+}
 
-	this.elm.classList.remove(this.classname, RESIZING);
-	if (this.originalPosition) {
-		this.elm.style.position = this.originalPosition;
+const DEFAULT_GRIP_SIZE = 10;
+
+function initGrips (rsz, gripSize, gripDirection) {
+	const createGrip = getGripCreator(gripSize || DEFAULT_GRIP_SIZE);
+
+	if (gripDirection) {
+		const {gripElm, moveHandler} = createGrip(direction[gripDirection]);
+		rsz.grip = gripElm;
+		rsz.elm.appendChild(gripElm);
+
+		const unbind = bindListener(gripElm, MOUSE_DOWN, (ev) => {
+			rsz.onResizeStart(ev, moveHandler);
+		});
+
+		rsz.destructionQueue.push(unbind);
+	}
+	else {
+		[
+			direction.topLeft,
+			direction.topRight,
+			direction.bottomRight,
+			direction.bottomLeft,
+		].forEach((gripDir) => {
+			const {propName, gripElm, moveHandler} = createGrip(gripDir);
+			rsz.grips[propName] = gripElm;
+			rsz.elm.appendChild(gripElm);
+
+			const unbind = bindListener(gripElm, MOUSE_DOWN, (ev) => {
+				rsz.onResizeStart(ev, moveHandler);
+			});
+
+			rsz.destructionQueue.push(unbind);
+		});
+	}
+}
+
+function initElm (rsz, elm) {
+	const position = elm.style.position || window.getComputedStyle(elm).position;
+	const box = elm.getBoundingClientRect();
+
+	if (position !== 'absolute') {
+		elm.style.position = 'absolute';
+		elm.style.top = box.top + px;
+		elm.style.left = box.left + px;
 	}
 
-	this.events = null;
-	this.elm = null;
-};
+	if (box.width < rsz.minWidth) {
+		elm.style.width = rsz.minWidth + px;
+	}
 
-Resizable.prototype.destroyGrips = function destroyGrips () {
-	this.hideGrips();
-	this.destructionQueue.forEach(callback => callback());
-	this.forEachGrip((grip) => {
-		this.elm.removeChild(grip);
+	if (box.height < rsz.minHeight) {
+		elm.style.height = rsz.minHeight + px;
+	}
+
+	elm.classList.add(rsz.classname);
+}
+
+function destroyGrips (rsz) {
+	rsz.hideGrips();
+	rsz.destructionQueue.forEach(callback => callback());
+	rsz.forEachGrip((grip) => {
+		rsz.elm.removeChild(grip);
 	});
-	this.grips = null;
-};
+	rsz.grips = null;
+}
